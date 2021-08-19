7bf5a6922e1b78ad0779d686c0204432bf3d49bd
Nikita Shpanko <shpancore@gmail.com>
2021-08-20 00:37:35 +0300

    init

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,7 @@
-import user from "../../assets/user.json";
-import stats from "../../assets/statistical-data.json";
-import friends from "../../assets/friends.json";
-import transactions from "../../assets/transactions.json";
-
-import Profile from "./Profile/Profile";
-import Statistics from "./Statistics/Statistics";
-import FriendList from "./FriendList/FriendList";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
 
 function App() {
   return (
     <>
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={stats} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
     </>
   );
 }
